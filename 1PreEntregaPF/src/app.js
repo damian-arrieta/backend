eb6ed32a7104051a71638de30d0868361ff91416
arrdamian <arrdamian@icloud.com>
2023-01-14 19:23:10 -0300

    Primera pre entrega PF

---
+++ 1PreEntregaPF/src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ProductManager = require('../src/main');
+
+const app = express();
+
+const products = new ProductManager('products.txt');
+
+app.get('/products', async (req, res) => {
+    const { limit } = req.query;
+    if (limit) {
+        const productos = await products.getProducts();
+        const productsLimited = products.slice(0, limit);
+        res.send(productsLimited);
+    }
+    else {
+        const showProducts = await products.getProducts();
+        res.send(showProducts);
+    }
+});
+
+app.get('/products/:pid', async (req, res) => {
+    const productId = req.params.pid;
+    const showProduct = await products.getProductById(parseInt(productId));
+    console.log(showProduct);
+    res.send(showProduct);
+})
+
+app.listen(8080, () => console.log('Servidor funcionando'));
