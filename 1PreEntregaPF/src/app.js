3ae2975feaf4736098c2be36063377cf86f3ed9a
arrdamian <arrdamian@icloud.com>
2023-01-15 21:08:23 -0300

    Se crean consignas

--- 1PreEntregaPF/src/app.js
+++ 1PreEntregaPF/src/app.js
@@ -1,28 +1,13 @@
-const express = require('express');
-const ProductManager = require('../src/main');
+import express from 'express';
+import productRouter from './routes/products.router.js';
+import cartRouter from './routes/carts.router.js';
 
 const app = express();
 
-const products = new ProductManager('products.txt');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.get('/products', async (req, res) => {
-    const { limit } = req.query;
-    if (limit) {
-        const productos = await products.getProducts();
-        const productsLimited = products.slice(0, limit);
-        res.send(productsLimited);
-    }
-    else {
-        const showProducts = await products.getProducts();
-        res.send(showProducts);
-    }
-});
+app.use('/api/products' , productRouter);
+app.use('/api/carts' , cartRouter);
 
-app.get('/products/:pid', async (req, res) => {
-    const productId = req.params.pid;
-    const showProduct = await products.getProductById(parseInt(productId));
-    console.log(showProduct);
-    res.send(showProduct);
-})
-
-app.listen(8080, () => console.log('Servidor funcionando'));
+const server = app.listen(8080 , () => console.log("Server activado"));
