9ef6887229604a7651e231582e653a6fd68f2943
arrdamian <arrdamian@icloud.com>
2023-01-23 12:37:03 -0300

    Ejercicios clase 2

--- DE1/main.js
+++ DE1/main.js
@@ -1,13 +1,12 @@
 class ProductManager {
-
     constructor(){
         this.products = [];
         this.id = 0;
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-        if (this.products.find(product => product.code !== code) || this.products.length === 0) {
-            if (title && description && price && thumbnail && code && stock) {
+    addProduct(title, description, price, thumbnail, code, stock){
+        if (this.products.find(product => product.code !== code) || this.products.length === 0){
+            if(title && description && price && thumbnail && code && stock){
                 const product = {
                     title: title,
                     description: description,
@@ -17,15 +16,13 @@ class ProductManager {
                     stock: stock,
                     id: this.id
                 };
-                this.id++;
+                this.id++
                 this.products.push(product);
+            } else {
+                console.log('ERROR: Todos los campos son obligatorios');
             }
-            else {
-                console.log("ERROR: Todos los campos son obligatorios");
-            }
-        }
-        else {
-            console.log("ERROR: El campo 'CODE' ya existe");
+        } else {
+            console.log('ERROR: El campo "CODE" ya existe');
         }
     }
 
@@ -33,25 +30,20 @@ class ProductManager {
         console.log(this.products);
     }
 
-    getProductById(id) {
-        if (this.products.find(product => product.id === id)) {
+    getProductById(id){
+        if (this.products.find(product => product.id === id)){
             let result = this.products.filter(product => product.id === id);
             console.log(result);
-        }
-        else {
-            console.log("ERROR: ID no encontrado");
+        } else {
+            console.log('ERROR: Not Found');
         }
     }
 }
 
 let products = new ProductManager();
 
-products.addProduct('CDJ 3000', 'Mejore su poder creativo con nuestro multirreproductor insignia evolucionado, el CDJ-3000', 1605000, 'https://deejaystore.com.ar/1119-large_default/pioneer-cdj-900-nexus.jpg', 'CDJ3000B', 8);
-
-products.addProduct('DJM-V10', 'Forja un nuevo sonido con la DJM-V10, una nueva generación de mezcladoras surgida de ideas innovadoras.', 1600000, 'https://www.pioneerdj.com/-/media/pioneerdj/images/products/mixer/djm-v10/djm-v10-main.png?h=1170&w=1029&hash=4BE6668E0CE85C3CB91B39887F6B84F1', 'DJMV10', 4);
-
+products.addProduct('Producto 1', 'Este es el producto 1', 1, 'Imagen producto 1', 'PRD1', 10);
+products.addProduct('Producto 2', 'Este es el producto 2', 2, 'Imagen producto 2', 'PRD2', 10);
 products.getProducts();
-
-products.getProductById(0);
-
-products.getProductById(5);
+products.getProductById(1);
+products.getProductById(2);
--- PracticaClases/Clase2.js
+++ PracticaClases/Clase2.js
@@ -18,3 +18,59 @@ console.log(obtener);
 
 let suma = obtener.reduce((valor, valor1) => valor + valor1);
 console.log(suma);
+
+class TicketManager {
+    constructor() {
+      this.eventos = [];
+      this._precioBaseDeGanancia = 0.15;
+      this._idAutoincrementable = 0;
+    }
+  
+    getEventos() {
+      return this.eventos;
+    }
+  
+    agregarEvento(nombre, lugar, precio, capacidad = 50, fecha = new Date()) {
+      const id = this._idAutoincrementable++;
+      const evento = {
+        id,
+        nombre,
+        lugar,
+        precio: precio + (precio * this._precioBaseDeGanancia),
+        capacidad,
+        fecha,
+        participantes: []
+      };
+      this.eventos.push(evento);
+    }
+  
+    agregarUsuario(idEvento, idUsuario) {
+      const evento = this.eventos.find(e => e.id === idEvento);
+      if (!evento) {
+        console.log("El evento no existe");
+        return;
+      }
+      if (evento.participantes.includes(idUsuario)) {
+        console.log("El usuario ya está registrado en este evento");
+        return;
+      }
+      evento.participantes.push(idUsuario);
+    }
+  
+    ponerEventoEnGira(idEvento, nuevaLocalidad, nuevaFecha) {
+      const evento = this.eventos.find(e => e.id === idEvento);
+      if (!evento) {
+        console.log("El evento no existe");
+        return;
+      }
+      const nuevoEvento = {
+        ...evento,
+        id: this._idAutoincrementable++,
+        lugar: nuevaLocalidad,
+        fecha: nuevaFecha,
+        participantes: []
+      };
+      this.eventos.push(nuevoEvento);
+    }
+  }
+  
