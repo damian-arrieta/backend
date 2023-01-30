bc1bdce880bc48040d74161599618c970c173d30
arrdamian <arrdamian@icloud.com>
2023-01-30 12:25:36 -0300

    Package.json agregado

--- PracticaClases/Clase4/Clase4.js
+++ PracticaClases/Clase4/Clase4.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/* const fs = require('fs');
 const date = new Date().toLocaleDateString();
 const time = new Date().toLocaleTimeString();
 
@@ -8,4 +8,39 @@ fs.writeFile('./Clase4.txt', `Fecha: ${date}, Hora: ${time}`, (error) => {
         if(error) return console.log(error);
         console.log(result);
     })
-})
+});*/
+
+import fs from 'fs';
+
+const leerArchivo = async (nombre) => {
+    try {
+        let contenidoSTR = await fs.promises.readFile(nombre, 'utf-8');
+        let contenidoObj = JSON.parse(contenidoSTR);
+        let size = (await fs.promises.stat(nombre)).size;
+        const info = {
+            'contenidoSTR': contenidoSTR,
+            'contenidoObj': contenidoObj,
+            'size': size
+        }
+        return info
+    } catch (error) {
+        throw new Error (error.message);
+    }
+}
+
+const resultado = async () => {
+    try {
+        const info = await leerArchivo('package.json');
+        console.log(info)
+    } catch (error) {
+        throw new Error (error);
+    }
+}
+
+(async function main() {
+    try {
+        await resultado();
+    } catch (error) {
+        console.error(error);
+    }
+})();
