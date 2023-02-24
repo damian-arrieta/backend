5f1ff7c934fdb6bc2576cf33bad03c0b36ba8561
arrdamian <arrdamian@icloud.com>
2023-02-24 16:10:51 -0300

    Finish ex Clase-4

--- 39715/Clase-4/index.js
+++ 39715/Clase-4/index.js
@@ -1,36 +1,52 @@
 const suma = (numero1, numero2) => {
-    if (numero1 === 0 || numero2 === 0) {
-        console.error('Operacion innecesaria');
-    }
-    else if (numero1 < 0 || numero2 < 0) {
-        console.error('La calculadora sólo debe devolver valores positivos');
-    } else {
-        console.log(numero1 + numero2);
-    }
+    return new Promise ((res, rej) => {
+        if (numero1 === 0 || numero2 === 0)
+            rej('Operacion innecesaria');
+        if (numero1 + numero2 < 0)
+            rej('La calculadora sólo debe devolver valores positivos');
+        res(console.log(numero1 + numero2));
+    })
 }
 
 const resta = (numero1, numero2) => {
-    if (numero1 === 0 || numero2 === 0) {
-        console.error('Operacion innecesaria');
-    }
-    else if (numero1 - numero2 < 0) {
-        console.error('La calculadora sólo debe devolver valores positivos');
-    } else {
-        console.log(numero1 - numero2);
-    }
+    return new Promise ((res, rej) => {
+        if (numero1 === 0 || numero2 === 0)
+            rej('Operacion innecesaria');
+        if (numero1 - numero2 < 0)
+            rej('La calculadora sólo debe devolver valores positivos');
+        res(console.log(numero1 - numero2));
+    })
 }
 
 const multiplicacion = (numero1, numero2) => {
-    if (numero1 === 0 || numero2 === 0) {
-        console.error('Operacion innecesaria');
-    }
-    else if (numero1 < 0 || numero2 < 0) {
-        console.error('La calculadora sólo debe devolver valores positivos');
-    } else {
-        console.log(numero1 * numero2);
-    }
+    return new Promise ((res, rej) => {
+        if (numero1 < 0 || numero2 < 0)
+            rej('Operacion innecesaria');
+        if (numero1 * numero2 < 0)
+            rej('La calculadora sólo debe devolver valores positivos');
+        res(console.log(numero1 * numero2));
+    })
+}
+
+const division = (numero1, numero2) => {
+    return new Promise ((res, rej) => {
+        if (numero1 < 0 || numero2 < 0)
+            rej('Operacion innecesaria');
+        if (numero1 / numero2 < 0)
+            rej('La calculadora sólo debe devolver valores positivos');
+        res(console.log(numero1 / numero2));
+    })
 }
 
 const calculos = async() => {
-    
+    try {
+        await suma(5, 0);
+        await resta(5, 3);
+        await multiplicacion(5, 3);
+        await division(5, 0);
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+calculos();
